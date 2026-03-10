842242909c2343c953037c56fa407c578f7a6d56
Carlos Lopes <carloslopes2507@gmail.com>
2026-03-10 23:41:14 +0000

    EmAndamento

---
+++ assignments/01-programming/01-primitives-variables-operators/03-fibonacci/submissions/Carlos.Lopes/Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro não negativo:  ");
+        int n = teclado.nextint();
+
+
+    }
+    public static int calcularFibonacci(int n){
+
+    }
+
+    public static long formatarSaida{
+
+    }
+    
+}
